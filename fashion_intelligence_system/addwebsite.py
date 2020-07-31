26fa4ec788cbb2fb1b3f85bbd72e2a5749ca1535
never2average <priyesh.sriv2017@gmail.com>
2020-07-31 15:26:55 +0530

    Make Email unique

--- fashion_intelligence_system/addwebsite.py
+++ fashion_intelligence_system/addwebsite.py
@@ -5,7 +5,10 @@ from models import Website
 def add_website(website_name, website_type):
     if website_type=="M" or website_type=="E":
         try:
-            Website(website_name, website_type).save()
+            Website(
+                website_name=website_name,
+                website_type=website_type
+            ).save()
             return json.dumps({"message": "AddingWebsiteSuccesful"}), 200
         except:
             return json.dumps({"message": "AddingWebsiteFailed"}), 401
